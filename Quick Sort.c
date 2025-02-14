9462b64efce5cb7fe08675d1623362b361f017ef
rakesh13012007 <jujjarapurakesh@gmail.com>
2025-02-14 22:27:44 +0530

    Quick Sort.c

--- Quick Sort.c
+++ Quick Sort.c
@@ -2,28 +2,28 @@
 int Partition(int A[],int key,int strt,int end){
 	int s=strt;
 	int e=end;
-	while(s<e){
-		while(A[s]<=key){
+	while(s<e){// runs upto strt less than end
+		while(A[s]<=key){// increment strt index upto element > key
 			s++;
 		}
-		while(A[e]>key){
+		while(A[e]>key){// decrement end upto element < key
 			e--;
 		}
 		if(s<e){
-			int temp = A[s];
+			int temp = A[s];// swaping the element at strt index and end index
 			A[s]=A[e];
 			A[e]=temp;
 		}
 	}
 	int temp = A[e];
-	A[e]=key;
-	A[strt]=temp;
-	return e;
+	A[e]=key;// key is swaped to end index
+	A[strt]=temp;// swap last element to start index
+	return e;// return key index
 }
 void Quick_Sort(int A[],int n,int strt,int end){
 	if(strt < end){
-		// key to divide
-		int p=A[strt];
+		// key to divide the array to 2 parts
+		int p=A[strt];// key be strting index
 		p=Partition(A,p,strt,end);
 		// from 0 to p(index) 
 		Quick_Sort(A,n,strt,p);
@@ -39,8 +39,9 @@ int main()
 	for(int i=0;i<n;i++){
 		scanf("%d",&A[i]);
 	}
-	Quick_Sort(A,n,0,n-1);
+	// Function call 
+	Quick_Sort(A,n,0,n-1);// time complexity is O(n)*log2(n)(Avgerage),O(n^2)(Worst Case)
 	for(int i=0;i<n;i++){
-		printf("%d ",A[i]);
+		printf("%d ",A[i]);// Printing sorted array
 	}
 }

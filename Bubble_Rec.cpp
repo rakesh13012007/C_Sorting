7405912c5d960e591c967245e84ef54bc79a2c23
rakesh13012007 <jujjarapurakesh@gmail.com>
2025-02-14 20:25:38 +0530

    Update Bubble_Rec.cpp

--- Bubble_Rec.cpp
+++ Bubble_Rec.cpp
@@ -1,21 +1,27 @@
 #include<stdio.h>
+// Printing array
 void Print(int A[],int n){
 	for(int i=0;i<n;i++){
 		printf("%d ",A[i]);
 	}
 }
+// Sorting
 void Bubble(int A[],int n,int i,int j){
+	//Base Case 1
 	if(i>=n){
 		return;
 	}
+	// Base Case 2
 	if(j>=n){
 		return;
 	}
+	// Swaping
     if( j<n-1 && A[j]>A[j+1]){
 	 int tmp = A[j];
 	 A[j]=A[j+1];
 	 A[j+1]=tmp;
 }
+	// Recursive call
 	 Bubble(A,n,i,j+1);
 	 Bubble(A,n,i+1,0);
 }
@@ -28,6 +34,8 @@ int main()
 		scanf("%d",&A[i]);
 	}
 	int i=0,j=0;
+	// function call
 	Bubble(A,n,i,j);
+	//Print call
 	Print(A,n);
 }

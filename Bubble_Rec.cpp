e8f974283efa17c5577a4019229e58076ef4ef35
rakesh13012007 <jujjarapurakesh@gmail.com>
2025-02-14 21:20:44 +0530

    Update Bubble_Rec.cpp

--- Bubble_Rec.cpp
+++ Bubble_Rec.cpp
@@ -1,28 +1,29 @@
 #include<stdio.h>
-// Printing array
+// Printing of sorted array
 void Print(int A[],int n){
 	for(int i=0;i<n;i++){
 		printf("%d ",A[i]);
 	}
 }
-// Sorting
+// Bubble Sort
 void Bubble(int A[],int n,int i,int j){
-	//Base Case 1
+	//Base Case 1 to Return from Outer loop
 	if(i>=n){
 		return;
 	}
-	// Base Case 2
+	// Base Case 2 to return from inner loop
 	if(j>=n){
 		return;
 	}
-	// Swaping
+	// Swaping of larger numbers to last of the array
     if( j<n-1 && A[j]>A[j+1]){
 	 int tmp = A[j];
 	 A[j]=A[j+1];
 	 A[j+1]=tmp;
 }
-	// Recursive call
+	// Recursive call for inner loop iteration
 	 Bubble(A,n,i,j+1);
+	// Recursive call for outer loop iteration
 	 Bubble(A,n,i+1,0);
 }
 int main()
@@ -34,8 +35,8 @@ int main()
 		scanf("%d",&A[i]);
 	}
 	int i=0,j=0;
-	// function call
-	Bubble(A,n,i,j);
-	//Print call
+	// Function for bubble sort
+	Bubble(A,n,i,j);// Time complexity is O(n^2)
+	//Printing of sorted array
 	Print(A,n);
 }
